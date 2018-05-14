da9576184028df83e43f850546de9de018d09d96
Ulrika Häggqvist <ulrika.haggqvist@arbetsformedlingen.se>
2018-05-14 09:58:37 +0200

    ny inbjudan

--- js/img.js
+++ js/img.js
@@ -7,7 +7,7 @@ var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
+    modalImg.src = "../../images/pdf-inbjudan_l.jpg";
     captionText.innerHTML = this.alt;
 };
 var span = document.getElementsByClassName("close")[0];
