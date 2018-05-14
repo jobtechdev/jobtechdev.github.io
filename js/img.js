513bd91c357d39ddde4187cd242d0d4fff15e633
Mats Löfstrand <mats.lofstrand@arbetsformedlingen.se>
2018-05-14 14:56:28 +0200

    added info about jobtechforum on index

--- js/img.js
+++ js/img.js
@@ -5,13 +5,14 @@ var modal = document.getElementById('myModal');
 var img = document.getElementById('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = "../../images/pdf-inbjudan_l.jpg";
-    captionText.innerHTML = this.alt;
+
 };
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-}
+
 };
